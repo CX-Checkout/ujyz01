0895e484578dc509321426fff54d9f45775536ae
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-11-15 11:41:36 +0000

    Wed Nov 15 11:41:36 GMT 2017

--- test/solutions/test-checkout.js
+++ test/solutions/test-checkout.js
@@ -15,7 +15,7 @@ exports['return the price of promoted product with single products'] = function 
     { products: 'A, A, A', result: 130},
     { products: '3A', result: 130},
     { products: 'AAA', result: 130},
-    { products: 'AAAA', result: 180},
+    { products: 'AAAA', result: 195},
     { products: 'AAAAA', result: 230},
     { products: 'AAAAAA', result: 260},
     { products: 'BB', result: 45},
@@ -33,7 +33,7 @@ exports['return the price of promoted product with single products'] = function 
 [
     { products: 'A B C D', result: 115},
     { products: 'A A A A B', result: 210},
-    { products: 'AAABC', result: 195}]
+    { products: 'AAABC', result: 180}]
     .forEach(function (scenario) {
         exports['return the price of multiple product when multiple products are bought - ' + scenario] = function (test) {
             test.equal(checkout(scenario.products), scenario.result);
