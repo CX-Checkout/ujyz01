1dd63addacd03354b49886a996089d7326ac42f6
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-11-15 11:31:19 +0000

    Wed Nov 15 11:31:19 GMT 2017

---
+++ test/solutions/test-checkout.js
@@ -0,0 +1,56 @@
+var checkout = require('../../lib/solutions/checkout');
+
+exports['return the price of one product'] = function (test) {
+    test.equal(checkout('A'), 50);
+    test.done();
+};
+
+exports['return the price of promoted product with single products'] = function (test) {
+    test.equal(checkout('AAAA'), 180);
+    test.done();
+};
+
+[
+    { products: 'A A A', result: 130},
+    { products: 'A, A, A', result: 130},
+    { products: '3A', result: 130},
+    { products: 'AAA', result: 130},
+    { products: 'AAAA', result: 180},
+    { products: 'AAAAA', result: 230},
+    { products: 'AAAAAA', result: 260},
+    { products: 'BB', result: 45},
+    { products: 'BBB', result: 75},
+    { products: 'CC', result: 40},
+    { products: 'DD', result: 30}
+]
+    .forEach(function (scenario) {
+        exports['return the price of one product when multiple products are bought - ' + scenario] = function (test) {
+            test.equal(checkout(scenario.products), scenario.result);
+            test.done();
+        };
+    }, this);
+
+[
+    { products: 'A B C D', result: 115},
+    { products: 'A A A A B', result: 210},
+    { products: 'AAABC', result: 195}]
+    .forEach(function (scenario) {
+        exports['return the price of multiple product when multiple products are bought - ' + scenario] = function (test) {
+            test.equal(checkout(scenario.products), scenario.result);
+            test.done();
+        };
+    }, this);
+
+
+//ERRORS
+[
+    { products: 'AAAAa', result: -1},
+    { products: '-AA', result: -1},
+    { products: 'AxA', result: -1}]
+    .forEach(function (scenario) {
+        exports['return errors on invalid request - ' + scenario] = function (test) {
+            test.equal(checkout(scenario.products), scenario.result);
+            test.done();
+        };
+    }, this);
+
